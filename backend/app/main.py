44424aebf9f99b529e654037d3121e8e80aacf36
MatthewRayUK <matthew.ray@hotmail.com>
2026-03-25 20:10:58 +0000

    Updated to sqlalchemy 2.0 and cleaned variable names

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,11 +1,12 @@
 from fastapi import Depends, FastAPI
 from sqlalchemy.orm import Session
 
+from .models import Todo
 from .repository import Repository, get_db
 
 app = FastAPI()
 
 
 @app.get("/")
-async def root(database: Session = Depends(get_db)):
+def get_todos(database: Session = Depends(get_db)) -> list[Todo]:
     return Repository(database).load_all_todos()
--- backend/app/tests/test_main.py
+++ backend/app/tests/test_main.py
@@ -1,15 +1,22 @@
-from ..models import ToDo
+from fastapi.testclient import TestClient
+
+from ..models import CreateTodo
 from ..repository import Repository
 
 
-def test_root_returns_all_todos(client, repository: Repository):
-    repository.save_todo(ToDo(id="1", task_name="Buy milk", is_done=False))
-    repository.save_todo(ToDo(id="2", task_name="Walk the dog", is_done=True))
+def test_root_returns_all_todos(client: TestClient, repository: Repository) -> None:
+    todo_1 = repository.save_todo(CreateTodo(task_name="Buy milk", is_done=False))
+    todo_2 = repository.save_todo(CreateTodo(task_name="Walk the dog", is_done=True))
 
     response = client.get("/")
 
     assert response.status_code == 200
     todos = response.json()
     assert len(todos) == 2
+    assert todos[0]["id"] == todo_1.id
     assert todos[0]["task_name"] == "Buy milk"
+    assert todos[0]["is_done"] is False
+
+    assert todos[1]["id"] == todo_2.id
     assert todos[1]["task_name"] == "Walk the dog"
+    assert todos[1]["is_done"] is True
